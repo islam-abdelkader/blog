f2b89d6fd1f2b034d111c6abe429ec3448001071
islam-abdelkader <islamabdelkader22@gmail.com>
2021-12-18 22:08:11 +0200

    d

--- routes/web.php
+++ routes/web.php
@@ -18,29 +18,20 @@ Route::get('/', function () {
 });
 /*
 Route::get('posts/{post}', function ($slug) {
-    // return $slug;
     $path = __DIR__."/../resources/posts/{$slug}.html";
 
-    abort_if(! file_exists($path),404);
-
-    $post = file_get_contents($path);
+    abort_if(! file_exists($path),40
+    $post = cache()->remember("posts.{$slug}", now()->addMinutes(2), function () use ($path) {
+        var_dump('file_get_content');
+        return file_get_contents($path);
+    });
     return view('post',[
         'post' =>  $post
     ]);
 })->where('post','[A-z_\-]+');
 */
 Route::get('posts/{post}', function ($slug) {
-    // return $slug;
-    $path = __DIR__."/../resources/posts/{$slug}.html";
-
-    abort_if(! file_exists($path),404);
-
-    $post = cache()->remember("posts.{$slug}", now()->addMinutes(2), function () use ($path) {
-        var_dump('file_get_content');
-        return file_get_contents($path);
-    });
-
-    return view('post',[
-        'post' =>  $post
-    ]);
+    abort_if(! file_exists($path = __DIR__."/../resources/posts/{$slug}.html"),404);
+    $post = cache()->remember("posts.{$slug}", now()->addMinutes(2), fn()=> file_get_contents($path));
+    return view('post',['post' =>  $post]);
 })->where('post','[A-z_\-]+');
