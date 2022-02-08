4421d70b30e1b5daed4e25b0811c29c7e37bd8af
islam-abdelkader <islamabdelkader22@gmail.com>
2022-02-08 12:39:43 +0200

    adding comment layout

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class LoginController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function __construct()
+    {
+
+        $this->middleware('auth')->only(['destroy']);
+
+    }
+
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('auth.login');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $attributes = $request->validate([
+            'email' =>"required|email", //|exists:users,email
+            'password' =>"required",
+        ]);
+
+        if (!auth()->attempt($attributes)) {
+            throw ValidationException::withMessages(['email'=>"Your Provided Credentials couldn't be verified"]);
+        }
+
+        session()->regenerate(); // session fixation
+
+        return redirect()->route('home')->with('success','Welcome Back!');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy()
+    {
+        auth()->logout();
+        return redirect()->route('home')->with('success', 'Goodbye!');
+    }
+}
---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,22 @@
+<article class="flex bg-gray-100 border border-gray-200 p-6 rounded-xl space-x-4">
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/60" alt="avatar" width="60" height="60" class="rounded-xl">
+    </div>
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">John Doe</h3>
+            <p class="text-xs">
+                Posted
+                <time>8 monthes ago</time>
+            </p>
+        </header>
+
+        <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel cumque nisi eius voluptatibus
+            est, animi, perferendis aspernatur totam magnam nemo maiores iste eligendi accusamus omnis
+            earum blanditiis voluptate at vitae officia rem laudantium atque labore explicabo! Obcaecati
+            ipsam nihil autem eveniet culpa? Mollitia labore, atque odit doloribus ad placeat saepe
+            nesciunt.
+        </p>
+    </div>
+</article>
