4ed5add85d9007306ff614f707cce3feaa1e4e57
islam-abdelkader <islamabdelkader22@gmail.com>
2022-02-10 11:32:27 +0200

    .

---
+++ app/Http/Controllers/NewsLetterController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\Newsletter;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+
+class NewsLetterController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Newsletter $newsletter)
+    {
+        request()->validate(['email' => 'required|email']);
+
+        try {
+
+            // (new Newsletter())->subscribe(request('email')); //first way
+            $newsletter->subscribe(request('email'));    //second way
+
+        } catch (\Exception$ex) {
+
+            throw ValidationException::withMessages([
+                'email' => 'this email could not be added to our newsletter list.',
+            ]);
+        }
+
+        return back()->with('success', 'You are now signed for our newsletter!');}
+
+    public function show($id)
+    {
+        //
+    }
+
+    public function edit($id)
+    {
+        //
+    }
+
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ resources/views/posts/_add-comment.blade.php
@@ -0,0 +1,27 @@
+@auth
+<x-panel>
+    <form action="{{ route('comments.store',$post->slug)}}" method="post">
+        @csrf
+        <header class="flex item-center">
+            <img src="https://i.pravatar.cc/60?u={{ auth()->id() }}" alt="user avatar" width="40" height="40"
+                class="rounded-full">
+            <h2 class="ml-4">want to participate?</h2>
+        </header>
+        <div class="mt-6">
+            <textarea name="body" id="" rows="5" class="w-full text-sm focus:outline-none focus:ring"
+                placeholder="Quick, thing of something to say!"></textarea>
+                @error('body')
+                @include('error')
+                @enderror
+        </div>
+        <footer class="flex justify-end mt-6 pt-6 border-t border-gray-200">
+            <x-btn-submit>Post</x-btn-submit>
+        </footer>
+    </form>
+</x-panel>
+@else
+<p class="font-semibold">
+    <a href="{{ route('register.create') }}" class="hover:underline">Register</a> or
+    <a href="{{ route('login.create') }}" class="hover:underline">Login</a> to Leave a comment!
+</p>
+@endauth
